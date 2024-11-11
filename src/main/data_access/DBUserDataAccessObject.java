670a0383b51a516d1facaccfd0f9fb8874e262c0
yuxi229 <yuxii.zhang@mail.utoronto.ca>
2024-11-10 20:36:31 -0500

    added to data_access files

--- src/main/data_access/DBUserDataAccessObject.java
+++ src/main/data_access/DBUserDataAccessObject.java
@@ -1,4 +1,19 @@
 package data_access;
 
-public class DBUserDataAccessObject {
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import entity.User;
+import entity.Room;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import use_case.navigation.NagivationDataAccessInterface;
+
+
+public class DBUserDataAccessObject implements NagivationDataAccessInterface{
 }
--- src/main/use_case/save_to_favorites/SaveFavoritesDataAccessInterface.java
+++ src/main/use_case/save_to_favorites/SaveFavoritesDataAccessInterface.java
@@ -1,4 +1,4 @@
 package use_case.save_to_favorites;
 
-public class SaveFavoritesDataAccessInterface {
+public interface SaveFavoritesDataAccessInterface {
 }
