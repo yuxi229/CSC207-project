90d102ddbad438de490ed385f1cd34ffe15b23ba
nicholaskoh <nicholas.koh@mail.utoronto.ca>
2024-11-11 10:35:16 -0500

    built navigationOutputData with estimatedtime and returns error message with roomcode if user input wrong roomcode

--- src/main/use_case/navigation/NavigationOutputData.java
+++ src/main/use_case/navigation/NavigationOutputData.java
@@ -1,4 +1,25 @@
 package use_case.navigation;
 
 public class NavigationOutputData {
+    private final String roomcode;
+    private final int estimatedTime; // in minutes or seconds
+    private final boolean error;
+
+    public NavigationOutputData(String roomcode, int estimatedTime, boolean error) {
+        this.roomcode = roomcode;
+        this.estimatedTime = estimatedTime;
+        this.error = error;
+    }
+
+    public String getRoomcode() {
+        return roomcode;
+    }
+
+    public int getEstimatedTime() {
+        return estimatedTime;
+    }
+
+    public boolean isError() {
+        return error;
+    }
 }
