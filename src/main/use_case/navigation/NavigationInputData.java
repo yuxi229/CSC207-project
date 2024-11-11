fa2a97db0a00768ad1699a2374e2688086933344
nicholaskoh <nicholas.koh@mail.utoronto.ca>
2024-11-11 10:36:05 -0500

    add NavigationInputData with departure and destination room codes

--- src/main/use_case/navigation/NavigationInputData.java
+++ src/main/use_case/navigation/NavigationInputData.java
@@ -1,4 +1,19 @@
 package use_case.navigation;
 
 public class NavigationInputData {
+    private final String departureRoomCode;
+    private final String destinationRoomCode;
+
+    public NavigationInputData(String departureRoomCode, String destinationRoomCode) {
+        this.departureRoomCode = departureRoomCode;
+        this.destinationRoomCode = destinationRoomCode;
+    }
+
+    public String getDepartureRoomCode() {
+        return departureRoomCode;
+    }
+
+    public String getDestinationRoomCode() {
+        return destinationRoomCode;
+    }
 }
