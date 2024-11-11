6a9cb30d6bad7d1efc44c511e40adbff91e4a2db
yuxi229 <yuxii.zhang@mail.utoronto.ca>
2024-11-11 09:55:00 -0500

    added to NavigationInputBoundary and NavigationOutputBoundary  files

--- src/main/use_case/navigation/NavigationInputBoundary.java
+++ src/main/use_case/navigation/NavigationInputBoundary.java
@@ -1,4 +1,13 @@
 package use_case.navigation;
 
+/**
+ * Input Boundary for actions which are related to logging in.
+ */
+
 public class NavigationInputBoundary {
+    /**
+     * Executes the navigation use case.
+     * @param NavigationInputData the input data
+     */
+    void execute(NavigationInputData navigationInputData);
 }
--- src/main/use_case/navigation/NavigationOutputBoundary.java
+++ src/main/use_case/navigation/NavigationOutputBoundary.java
@@ -1,4 +1,19 @@
 package use_case.navigation;
 
+/**
+ * Output Data for the Navigation Use Case.
+ */
 public class NavigationOutputBoundary {
+
+    private final String roomcode;
+    private final boolean useCaseFailed;
+
+    public NavigationOutputData(String roomcode, boolean useCaseFailed) {
+        this.roomcode = roomcode;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public String getRoomcode() {
+        return roomcode;
+    }
 }
