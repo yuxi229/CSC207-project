d81aa81f534ffd56a097186e22191e4e688f626b
39kie <158347930+39kie@users.noreply.github.com>
2024-11-11 09:56:57 -0500

    Implemented NavigationDataAccessInterface

---
+++ src/main/use_case/navigation/NavigationDataAccessInterface.java
@@ -0,0 +1,12 @@
+package use_case.navigation;
+
+/**
+ * The Navigation Data Access Use Case.
+ */
+public interface NavigationDataAccessInterface {
+    /**
+     * Execute the Navigation Data Acess Use Case
+     * @param navigationInputData the input data for this use case.
+     */
+    void execute(NavigationInputData navigationInputData);
+}
