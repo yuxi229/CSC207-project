615e6712c8dc3b1aee900e226d27701adb1ccd56
yuxi229 <yuxii.zhang@mail.utoronto.ca>
2024-11-11 10:17:26 -0500

    changed NavigationInputBoundary to interface  files

--- src/main/use_case/navigation/NavigationInputBoundary.java
+++ src/main/use_case/navigation/NavigationInputBoundary.java
@@ -4,7 +4,7 @@ package use_case.navigation;
  * Input Boundary for actions which are related to logging in.
  */
 
-public class NavigationInputBoundary {
+public interface NavigationInputBoundary {
     /**
      * Executes the navigation use case.
      * @param NavigationInputData the input data
