eba25c43d74fc5529d1ec745d6d198245272e7a0
yuxi229 <yuxii.zhang@mail.utoronto.ca>
2024-11-11 21:26:23 -0500

    added to DBRoomDataAccessObject

--- src/main/data_access/DBRoomDataAccessObject.java
+++ src/main/data_access/DBRoomDataAccessObject.java
@@ -1,5 +1,7 @@
 package data_access;
 
+import java.io.IOException;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,7 +18,58 @@ import use_case.navigation.NavigationDataAccessInterface;
  * The DAO for room data.
  */
 public class DBRoomDataAccessObject implements NavigationDataAccessInterface{
+    private static final int SUCCESS_CODE = 200;
+    private static final String CONTENT_TYPE_LABEL = "Content-Type";
+    private static final String CONTENT_TYPE_JSON = "application/json";
+    private static final String STATUS_CODE_LABEL = "status_code";
     private static final String ROOMCODE = "roomcode";
     private static final String MESSAGE = "message";
+    private final RoomFactory roomFactory;
+
+    public DBRoomDataAccessObject(RoomFactory roomFactory) {
+        this.roomFactory = roomFactory;
+    }
+
+    @Override
+    public Room get(String roomCode) {
+
+        final Response response = client.newCall(request).execute();
+
+        final JSONObject responseBody = new JSONObject(response.body().string());
+
+        if (responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE) {
+            final JSONObject userJSONObject = responseBody.getJSONObject("roomcode");
+            final String name = userJSONObject.getString(ROOMCODE);
+
+            return roomFactory.create(roomCode);
+        }
+        else {
+            throw new RuntimeException(responseBody.getString(MESSAGE));
+        }
+    }
+        catch (IOException | JSONException ex) {
+        throw new RuntimeException(ex);
+    }
+}
+
+@Override
+public boolean existsByRoomCode(String roomCode) {
+    final OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+    final Request request = new Request.Builder()
+            .url(String.format("http://vm003.teach.cs.toronto.edu:20112/checkIfUserExists?username=%s", username))
+            .addHeader(CONTENT_TYPE_LABEL, CONTENT_TYPE_JSON)
+            .build();
+    try {
+        final Response response = client.newCall(request).execute();
+
+        final JSONObject responseBody = new JSONObject(response.body().string());
+
+        return responseBody.getInt(STATUS_CODE_LABEL) == SUCCESS_CODE;
+    }
+    catch (IOException | JSONException ex) {
+        throw new RuntimeException(ex);
+    }
+}
 
 }
