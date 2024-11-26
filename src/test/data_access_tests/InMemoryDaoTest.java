46d0276bd414d034bd8ddc9c5a6ed5baf92464c9
Simon <bosen.xu@mail.utoronto.ca>
2024-11-26 12:03:16 -0500

    - Structured tests folder to match source folder

---
+++ src/test/data_access_tests/InMemoryDaoTest.java
@@ -0,0 +1,4 @@
+package data_access_tests;
+
+public class InMemoryDaoTest {
+}
--- src/test/use_case_tests/enter_departure/EnterDepartureInteractorTest.java
+++ src/test/use_case_tests/enter_departure/EnterDepartureInteractorTest.java
@@ -1,4 +1,4 @@
-package enter_departure;
+package use_case_tests.enter_departure;
 
 public class EnterDepartureInteractorTest {
 }
