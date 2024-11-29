076a86453755e44aaa02ec32146cf67c4c6ed45f
Simon <bosen.xu@mail.utoronto.ca>
2024-11-29 16:16:23 -0500

    - Started implementing NavigationFacade to put the parts together

---
+++ src/main/app/NavigationFacade.java
@@ -0,0 +1,47 @@
+package app;
+
+import data_access.LocationDataAccess;
+import data_access.MapLocationDataAccess;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.beginnavigation.BeginNavigationViewModel;
+import interface_adapter.inputrooms.InputRoomsController;
+import interface_adapter.inputrooms.InputRoomsPresenter;
+import interface_adapter.inputrooms.InputRoomsViewModel;
+import use_case.navigation.JgraphtPathFinder;
+import use_case.navigation.NavigationInputBoundary;
+import use_case.navigation.NavigationInteractor;
+import use_case.navigation.NavigationOutputBoundary;
+import use_case.navigation.PathFinder;
+
+/**
+ * A facade taking care of the subsystem of setting up the map navigation use case.
+ */
+public class NavigationFacade {
+    /**
+     * Sets up the map navigation use case.
+     * @param locationData the location data access object
+     * @param mapLocationData the map location data access object
+     * @param viewManagerModel the view manager model
+     */
+    public static void setup(LocationDataAccess locationData, MapLocationDataAccess mapLocationData,
+                      ViewManagerModel viewManagerModel) {
+        final NavigationOutputBoundary naviPresenter = setUpPresenter(viewManagerModel);
+        final NavigationInputBoundary navigationUseCaseInteractor = setUpInteractor(locationData, mapLocationData,
+                naviPresenter);
+        final InputRoomsController inputRoomsController = new InputRoomsController(navigationUseCaseInteractor);
+        // TODO: Implement the rest of the setup as more components are completed
+    }
+
+    private static NavigationInputBoundary setUpInteractor(LocationDataAccess locationData,
+                                                           MapLocationDataAccess mapLocationData,
+                                                           NavigationOutputBoundary naviPresenter) {
+        final PathFinder pathFinder = new JgraphtPathFinder(locationData, mapLocationData);
+        return new NavigationInteractor(locationData, pathFinder, naviPresenter);
+    }
+
+    private static NavigationOutputBoundary setUpPresenter(ViewManagerModel viewManagerModel) {
+        final InputRoomsViewModel inputRoomsViewModel = new InputRoomsViewModel();
+        final BeginNavigationViewModel beginNavigationViewModel = new BeginNavigationViewModel();
+        return new InputRoomsPresenter(viewManagerModel, beginNavigationViewModel, inputRoomsViewModel);
+    }
+}
