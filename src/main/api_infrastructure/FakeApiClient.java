59a54527e90763c21584969da45b17a0388f0d2e
Simon <bosen.xu@mail.utoronto.ca>
2024-11-29 14:31:53 -0500

    - Created fake api client for testing purposes

---
+++ src/main/api_infrastructure/FakeApiClient.java
@@ -0,0 +1,38 @@
+package api_infrastructure;
+
+import java.util.Map;
+
+/**
+ * Fake implementation of the APIClient interface for testing purposes.
+ */
+public class FakeApiClient implements APIClient {
+    @Override
+    public Map<String, Object> fetchRooms() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> fetchCorridors() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> fetchWashrooms() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> fetchValves() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> fetchStairs() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> fetchElevators() {
+        return null;
+    }
+}
