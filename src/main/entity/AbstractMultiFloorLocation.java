25825b8327c5f9714e7398b1a9d620084fcf7d65
Simon <bosen.xu@mail.utoronto.ca>
2024-11-29 13:15:46 -0500

    - Deleted unused interface MultiFloorLocation.java

--- src/main/entity/AbstractMultiFloorLocation.java
+++ src/main/entity/AbstractMultiFloorLocation.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Abstract class for locations that span multiple floors (e.g., Stairs, Elevators).
  */
-public abstract class AbstractMultiFloorLocation extends AbstractLocation implements MultiFloorLocation {
+public abstract class AbstractMultiFloorLocation extends AbstractLocation {
     private final List<Integer> floors;
 
     AbstractMultiFloorLocation(String id, List<String> connectedLocations, int size, List<Integer> floors) {
