f313b21b0c897760c3cbdd643046ec5c3aa22e08
yuxi229 <yuxii.zhang@mail.utoronto.ca>
2024-12-01 15:43:28 -0500

    Update InputRoomsController.java

--- src/main/interface_adapter/inputrooms/InputRoomsController.java
+++ src/main/interface_adapter/inputrooms/InputRoomsController.java
@@ -1,28 +1,15 @@
 package interface_adapter.inputrooms;
 
-import use_case.navigation.NavigationFacade;
 import use_case.navigation.NavigationInputBoundary;
-import use_case.navigation.NavigationInputData;
 
 /**
  * The controller for the Login Use Case.
  */
 public class InputRoomsController {
 
-    private NavigationInputBoundary navigationUseCaseInteractor;
+    private NavigationInputBoundary navigationFacade;
 
-    public InputRoomsController(NavigationInputBoundary navigationUseCaseInteractor) {
-        this.navigationUseCaseInteractor = navigationUseCaseInteractor;
-    }
-
-    /**
-     * Executes the Login Use Case.
-     * @param departure_room the roomCode of the departure room
-     * @param destination_room the roomCode of the destination room
-     */
-    public void execute(String departure_room, String destination_room) {
-        final NavigationInputData navigationInputData = new NavigationInputData(departure_room, destination_room);
-        NavigationFacade navigationFacade = new NavigationFacade(navigationInputData);
-        navigationFacade.execute(navigationInputData);
+    public InputRoomsController(NavigationInputBoundary navigationFacade) {
+        this.navigationFacade = navigationFacade;
     }
 }
