3b36ff8af21063e8777186adb432d3afe08df6fd
Simon <bosen.xu@mail.utoronto.ca>
2024-11-30 19:03:24 -0500

    - Finished basic implementation of make instructions use case

--- src/main/use_case/instructions/InstructionsInteractor.java
+++ src/main/use_case/instructions/InstructionsInteractor.java
@@ -1,5 +1,24 @@
 package use_case.instructions;
 
-public class InstructionsInteractor {
+import java.util.List;
 
+import use_case.navigation.maplocation.MapLocation;
+
+/**
+ * The interactor for the make instructions use case.
+ */
+public class InstructionsInteractor {
+    private final InstructionsOutputBoundary presenter;
+
+    public InstructionsInteractor(InstructionsOutputBoundary presenter) {
+        this.presenter = presenter;
+    }
+
+    /**
+     * Make the instructions and send it to the presenter.
+     * @param path A list of map locations to make instructions for.
+     */
+    public void execute(List<MapLocation> path) {
+        presenter.presentInstructions(new BasicTextInstructions(path));
+    }
 }
